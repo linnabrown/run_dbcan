188d63c4da2cfccc2140fc1ebfb9f8f0b507b5ec
linnabrown <lehuang@longleaf-login4.its.unc.edu>
2021-12-20 14:27:12 -0500

    rm Hotpep

--- Hotpep/hotpep_data.py
+++
@@ -1,8 +0,0 @@
-# resolve path to Hotpep data file,
-# both when installed as a package, and in development
-
-import os.path
-import pkg_resources
-
-def hotpep_data_path(*args):
-    return pkg_resources.resource_filename('Hotpep', os.path.join(*args))
